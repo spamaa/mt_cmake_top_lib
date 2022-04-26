f4f565249da1b256cc8e9d96cdbc45a04e02a993
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-04-26 08:23:53 +0200

    initial commit

---
+++ CMakeLibTop.cpp
@@ -0,0 +1,17 @@
+﻿// CMaleLibTop.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeLibTop.h"
+#include <iostream>
+
+
+namespace CMakeLibTop
+{
+	void About()
+	{
+		::std::cout << "Hello from CMakeLibTop" << ::std::endl;
+		::std::cout << __FUNCTION__ << ":" << __FILE__ << ":" << __LINE__ << std::endl;
+		::std::cout << "===" << ::std::endl;
+	}
+}
+
