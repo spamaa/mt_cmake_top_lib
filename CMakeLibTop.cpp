d6af4ef609f3856b2df0f9c4a0ea0e0fb5c91fca
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-04-28 09:28:01 +0200

    some work change

--- CMakeLibTop.cpp
+++ CMakeLibTop.cpp
@@ -12,6 +12,7 @@ namespace CMakeLibTop
 		::std::cout << "Hello from CMakeLibTop" << ::std::endl;
 		::std::cout << __FUNCTION__ << ":" << __FILE__ << ":" << __LINE__ << std::endl;
 		::std::cout << "=== 1.0.2 ===" << ::std::endl;
+		::std::cout << "This is some dev change 01" << ::std::endl;
 	}
 }
 
