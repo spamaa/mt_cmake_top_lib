97be1e90f684aa977d922cb2a8c74df1d67773ef
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-04-28 13:37:03 +0200

    change version to 1.0.3

--- include/CMakeLibTop.h
+++ include/CMakeLibTop.h
@@ -1,7 +1,7 @@
 ﻿// CMaleLibTop.h : Include file for standard system include files,
 // or project specific include files.
 
-// This is header for 1.0.2 dev change 04
+// This is header for 1.0.3 master change 05
 
 #pragma once
 
