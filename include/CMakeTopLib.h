305decac66de5c237f2e96faa014152d9c40540e
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-09 11:54:09 +0200

    move deps to static lib from private to public

--- include/CMakeTopLib.h
+++ include/CMakeTopLib.h
@@ -1,7 +1,7 @@
 ﻿// CMaleLibTop.h : Include file for standard system include files,
 // or project specific include files.
 
-// This is header for 2.0.1
+// This is header for 2.0.2
 
 #pragma once
 
