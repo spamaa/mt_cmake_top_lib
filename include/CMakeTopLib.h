976172cecafa2c72805f38470fa799f978aaa538
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-11 12:04:20 +0200

    fix in CMakeLists.txt

--- include/CMakeTopLib.h
+++ include/CMakeTopLib.h
@@ -1,7 +1,7 @@
 ﻿// CMaleLibTop.h : Include file for standard system include files,
 // or project specific include files.
 
-// This is header for 2.1.3
+// This is header for 2.1.4
 
 #pragma once
 
